599bd902037b9228c8003a0e8eb0153cf2ef315a
Rahul Dinesh <94444265+kadavilrahul@users.noreply.github.com>
2025-05-21 11:04:03 +0530

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,3 +1,2 @@
-ssh-keygen -f '/root/.ssh/known_hosts' -R 'your_ip'
-rsync -avz /website_backups/postgres your_ip:/website_backups
+rsync -avz /website_backups/ your_ip:/website_backups
 your_password
